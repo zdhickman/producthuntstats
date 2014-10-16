62207bb7c2234ec8c6b5d38aa514b880ca950f0a
Zack Hickman <zdhickman@gmail.com>
2014-10-16 17:01:55 -0400

    default clojure project

---
+++ test/producthuntstats/core_test.clj
@@ -0,0 +1,7 @@
+(ns producthuntstats.core-test
+  (:require [clojure.test :refer :all]
+            [producthuntstats.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
