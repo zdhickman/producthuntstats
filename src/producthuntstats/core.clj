9fe6de09326ef6d741297beeed91346f38c42445
Zack Hickman <zdhickman@gmail.com>
2014-10-17 18:21:28 -0400

    grab links

--- src/producthuntstats/core.clj
+++ src/producthuntstats/core.clj
@@ -4,7 +4,7 @@
 
 (def ^:dynamic base-url "http://www.producthunt.com/?page=")
 (def last-page 164) ; actually last page + 
-(def num-fields 5)
+(def num-fields 6)
 
 (defn gen-page [number]
   (str base-url number))
@@ -15,7 +15,7 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(def fields [:votes :title :maker :description :comments])
+(def fields [:votes :link :title :maker :description :comments])
 
 (defn zipmap-fields [l]
   (zipmap fields l))
@@ -28,14 +28,15 @@
 
 (defn add-missing-makers [l]
   (if (empty? l) l
-    (if (number? (String->Number (nth l 3)))
+    (if (number? (String->Number (nth l 4)))
       (cons (nth l 0)
         (cons (nth l 1)
           (cons false
             (cons (nth l 2)
               (cons (nth l 3)
-                (add-missing-makers (rest (rest (rest (rest l))))))))))
-      (concat (take 5 l) (add-missing-makers (drop 5 l))))))
+                (cons (nth l 4)
+                (add-missing-makers (rest (rest (rest (rest (rest l))))))))))))
+      (concat (take num-fields l) (add-missing-makers (drop num-fields l))))))
 
 (defn maybe-replace-maker-span [item]
   (if (= item "\n          The maker of this product joined the comments\n        ")
@@ -45,10 +46,22 @@
 (defn smart-partition [numba l]
   (partition numba (map maybe-replace-maker-span (add-missing-makers l))))
 
+(defn text* [all-things]
+  (if (empty? all-things) all-things
+    (let [one-thing (first all-things)
+          maybe-href (:href (:attrs one-thing))]
+      (if (not (not maybe-href))
+        (cons maybe-href 
+          (cons (html/text one-thing)
+            (text* (rest all-things))))
+        (cons (html/text one-thing)
+          (text* (rest all-things)))))))
+
+
 (defn page-products [page]
   (map zipmap-fields
     (smart-partition num-fields
-      (map html/text 
+      (text* 
         (html/select page 
                       #{[:a.post-url.title] ; title
                         [:span.post-tagline.description] ; description
