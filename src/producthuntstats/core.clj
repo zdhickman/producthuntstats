be06badacf494190e6691aafb9c264af845b99ca
Nat Dempkowski <natdempk@gmail.com>
2014-10-16 23:53:39 -0400

    generate pages to scrape

--- src/producthuntstats/core.clj
+++ src/producthuntstats/core.clj
@@ -1,5 +1,15 @@
 (ns producthuntstats.core
-  (:gen-class))
+  (:gen-class)
+  (:require [net.cgrand.enlive-html :as html]))
+
+(def ^:dynamic base-url "http://www.producthunt.com/?page=")
+(def last-page 165) ; actually last page + 1
+
+(defn gen-page [number]
+  (str base-url number))
+
+(defn all-pages []
+  (map gen-page (range last-page)))
 
 (defn -main
   "I don't do a whole lot ... yet."
