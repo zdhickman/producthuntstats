5c1694aac2b9735545e407f98b6146fbfb08012b
Nat Dempkowski <natdempk@gmail.com>
2014-10-17 02:01:33 -0400

    Add function to scrape most basic product data

--- src/producthuntstats/core.clj
+++ src/producthuntstats/core.clj
@@ -3,7 +3,7 @@
   (:require [net.cgrand.enlive-html :as html]))
 
 (def ^:dynamic base-url "http://www.producthunt.com/?page=")
-(def last-page 165) ; actually last page + 1
+(def last-page 164) ; actually last page + 1
 
 (defn gen-page [number]
   (str base-url number))
@@ -11,7 +11,30 @@
 (defn all-pages []
   (map gen-page (range last-page)))
 
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(def fields [:votes :title :description :comments])
+
+(defn zipmap-fields [l]
+  (zipmap fields l))
+
+(defn page-products [page]
+  (map zipmap-fields
+    (partition 4 
+      (map html/text 
+        (html/select page 
+                      #{[:a.post-url.title] ; title
+                        [:span.post-tagline.description] ; description
+                        [:span.vote-count] ; votes
+                        [:p.comment-count] ; comments
+                        ;[] ; TODO: founder boolean
+                        })))))
+
+(defn fetch-products []
+  (map page-products (map fetch-url (all-pages))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Scrape Product Hunt archive data"
   [& args]
   (println "Hello, World!"))
